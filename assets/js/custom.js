4f0935669f81bbe5d089f7f2ffa7aa076cb96748
BOUNTY707 <mehrobakooo@gmail.com>
2024-04-03 03:04:26 +0500

    50% tayyor

---
+++ assets/js/custom.js
@@ -0,0 +1,66 @@
+// Economic slider
+$('.economic-content').slick({
+    dots: false,
+    infinite: false,
+    speed: 400,
+    slidesToShow: 4,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+
+// Company logo slider
+$('.company-logo-links').slick({
+    slidesToShow: 8.8,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
