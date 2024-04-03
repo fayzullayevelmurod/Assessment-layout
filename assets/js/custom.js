12aaf834cbe2f8fb4b87c413c8e70daab40e2b1a
BOUNTY707 <mehrobakooo@gmail.com>
2024-04-04 01:00:30 +0500

    project yakunlandi

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,40 +1,54 @@
-// Header select
-$('.header-select-btn').click(function () {
-    $('.header-select-value').toggleClass('active');
+$(document).ready(function() {
+    // Header select
+    $('.header-select-btn').click(function () {
+        let headerSelectValue = $(this).siblings('.header-select-value');
+        headerSelectValue.toggleClass('active');
+    });
+
+    $('.header-select-value span').click(function () {
+        let text = $(this).text();
+        $(this).closest('.header-select').find('.header-select-btn span').text(text);
+        $(this).closest('.header-select-value').removeClass('active');
+    });
+    
+    // Header menu
+    $('.header-bars').click(function () {
+        $('.header-mob').toggleClass('active');
+    })
+    $('.menu-close').click(function () {
+        $('.header-mob').removeClass('active');
+    })
+    $('.header-mob-bg').click(function () {
+        $('.header-mob').removeClass('active');
+    })
 });
-$('.header-select-value span').click(function () {
-    var text = $(this).text(); // Tıklanan span'in metnini al
-    $('.header-select-btn span').text(text); // Bu metni .header-select-btn içindeki span'e yerleştir
-    $('.header-select-value').removeClass('active'); // header-select-value öğesinden 'active' sınıfını kaldır
-});
+
 
 
 
 // Economic slider
 $('.economic-content').slick({
     dots: false,
-    infinite: false,
+    infinite: true,
     speed: 400,
     slidesToShow: 4,
     responsive: [
         {
-            breakpoint: 1024,
+            breakpoint: 1250,
             settings: {
                 slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
+                slidesToScroll: 1,
             }
         },
         {
-            breakpoint: 600,
+            breakpoint: 991,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToScroll: 1
             }
         },
         {
-            breakpoint: 480,
+            breakpoint: 300,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
@@ -52,26 +66,31 @@ $('.company-logo-links').slick({
     autoplaySpeed: 800,
     responsive: [
         {
-            breakpoint: 1024,
+            breakpoint: 1700,
             settings: {
+                centerMode: true,
+                slidesToShow: 6
+            }
+        },
+        {
+            breakpoint: 1100,
+            settings: {
+                centerMode: true,
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 776,
+            settings: {
+                centerMode: true,
                 slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
             }
         },
         {
-            breakpoint: 600,
+            breakpoint: 576,
             settings: {
+                centerMode: true,
                 slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
             }
         }
     ]
